5967167ba1aa643f630948577fdaf72bb176f31b
apergom <aperezg@iesrafaelalberti.es>
2026-06-16 11:52:31 +0200

    Aplicacion terminada a falta de resumen

--- src/main/kotlin/app/PhotoRenamerApp.kt
+++ src/main/kotlin/app/PhotoRenamerApp.kt
@@ -19,9 +19,12 @@ class PhotoRenamerApp {
         // Procesar las fotos validas y generar el resultado con comandos y estadisticas.
         val processor = PhotoProcessor()
 
+        val result = processor.process(trip)
 
         // Escribir el script <lugar>.sh con los comandos mv generados.
         val writer = ScriptWriter()
+        writer.write(trip.place,result)
+
         // Mostrar por consola el resumen de fotos leidas, correctas y erroneas.
 
         // Si aplica la ampliacion de base de datos, guardar el resumen del procesamiento.
--- src/main/kotlin/domain/port/ScriptWriter.kt
+++ src/main/kotlin/domain/port/ScriptWriter.kt
@@ -1,13 +1,25 @@
 package org.iesra.domain.port
 
+import org.iesra.domain.model.ProcessingResult
 import java.nio.file.Files
 import java.nio.file.Paths
 
 class ScriptWriter {
-    fun write(place: String, commands: List<String>) {
+    fun write(place: String, result: ProcessingResult) {
+
+        val commands = result.commands
 
         val sb = StringBuilder()
 
 
+        for (command in commands) {
+            sb.append("$command\n")
+        }
+
+        val scriptPath = Paths.get("${place}.sh")
+        Files.write(scriptPath, sb.toString().toByteArray())
+
+        val scriptFile = scriptPath.toFile()
+        scriptFile.setExecutable(true)
     }
 }
