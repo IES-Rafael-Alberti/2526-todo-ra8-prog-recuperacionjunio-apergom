5eb46c2034d748b02a09ec97f2e0f1c3ec79b3c9
apergom <aperezg@iesrafaelalberti.es>
2026-06-16 11:39:49 +0200

    estadisticas implementadas

--- src/main/kotlin/domain/model/ProcessingResult.kt
+++ src/main/kotlin/domain/model/ProcessingResult.kt
@@ -2,5 +2,7 @@ package org.iesra.domain.model
 
 data class ProcessingResult (
     val commands: List<String>,
-    val reflexStats: PhotoTypeStats
+    val reflexStats: PhotoTypeStats,
+    val smartphoneStats: PhotoTypeStats,
+    val errors: List<String>
 )
--- src/main/kotlin/domain/port/PhotoProcessor.kt
+++ src/main/kotlin/domain/port/PhotoProcessor.kt
@@ -1,14 +1,32 @@
 package org.iesra.domain.port
 
 import org.iesra.domain.model.PhotoFile
+import org.iesra.domain.model.PhotoTypeStats
 import org.iesra.domain.model.ProcessingResult
 import org.iesra.domain.model.ReflexPhotoFile
 import org.iesra.domain.model.SmartphonePhotoFile
 import org.iesra.domain.model.TripInput
-import java.io.File
 
 class PhotoProcessor {
-  fun process(input: TripInput): ProcessingResult {}
+    private val errors: MutableList<String> = mutableListOf()
+  fun process(input: TripInput): ProcessingResult {
+
+      val reflexPhotos = buildValidReflexPhotos(input.reflexFiles)
+      val smartphonePhotos = buildValidSmartphonePhotos(input.smartphoneFiles)
+
+      val reflexStats = PhotoTypeStats(
+          read = input.reflexFiles.size,
+          correct = reflexPhotos.size,
+          errors = input.reflexFiles.size - reflexPhotos.size
+      )
+
+      val smartphoneStats = PhotoTypeStats(
+          read = input.smartphoneFiles.size,
+          correct = smartphonePhotos.size,
+          errors = input.smartphoneFiles.size - smartphonePhotos.size
+      )
+
+  }
 
 
     private fun buildValidReflexPhotos(names: List<String>): List<PhotoFile>{
@@ -34,4 +52,3 @@ class PhotoProcessor {
         }
         return validPhotos
     }
-}
